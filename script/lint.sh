d8bdbf14b654f99710109b5761b41a9a8fdaf2f2
Ozzy <cfhamlet@gmail.com>
2020-04-24 16:01:01 +0800

    add: startproject can create python package

--- script/lint.sh
+++ script/lint.sh
@@ -10,6 +10,8 @@ set -x
 
 pip install -r requirements/requirements-lint.txt
 
-${PREFIX}autoflake --in-place --recursive --remove-all-unused-imports --remove-unused-variables src tests
-${PREFIX}black --exclude=".pyi$" src tests
-${PREFIX}isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --apply src tests
+FILES="src tests setup.py"
+
+${PREFIX}autoflake --in-place --recursive --remove-all-unused-imports --remove-unused-variables ${FILES}
+${PREFIX}black --exclude=".pyi$" ${FILES}
+${PREFIX}isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --apply ${FILES}
